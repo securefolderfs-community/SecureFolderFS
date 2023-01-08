eee70e5ed07b253c0d9d4feb9fc0b2b866c5c36a
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2023-01-08 11:47:08 +0100

    Fix Tmds.Fuse submodule

--- lib/Tmds.Fuse/src/Tmds.Fuse/MountOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace Tmds.Fuse
-{
-    public class MountOptions
-    {
-        public bool SingleThread { get; set; } = false;
-    }
-}
--- lib/Tmds.Fuse/src/Tmds.Fuse/ReadDirFlags.cs
+++
@@ -1,6 +0,0 @@
-using System;
-
-namespace Tmds.Fuse
-{
-    public enum ReadDirFlags { }
-}
