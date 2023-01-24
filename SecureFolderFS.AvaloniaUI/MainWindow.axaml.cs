d6666aab05bfbf358721e142c62ff970d5764c33
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2023-01-24 16:43:38 +0100

    Add Avalonia project

---
+++ SecureFolderFS.AvaloniaUI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SecureFolderFS.AvaloniaUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
