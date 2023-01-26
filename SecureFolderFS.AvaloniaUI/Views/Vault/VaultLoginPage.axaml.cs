4233c7162e981984e94f7f5ca11cc5fa01c42527
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2023-01-26 14:09:36 +0100

    Add VaultLoginPage

--- SecureFolderFS.AvaloniaUI/Views/Vault/VaultLoginPage.axaml.cs
+++ SecureFolderFS.AvaloniaUI/Views/Vault/VaultLoginPage.axaml.cs
@@ -1,19 +1,84 @@
+using System.Text;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using FluentAvalonia.UI.Navigation;
+using SecureFolderFS.AvaloniaUI.UserControls;
+using SecureFolderFS.Sdk.ViewModels.Pages.Vault;
+using SecureFolderFS.Sdk.ViewModels.Vault.LoginStrategy;
+using SecureFolderFS.UI.AppModels;
 
 namespace SecureFolderFS.AvaloniaUI.Views.Vault
 {
-    public sealed partial class VaultLoginPage : UserControl
+    internal sealed partial class VaultLoginPage : Page
     {
+        private Button? _continueButton;
+        private TextBox? _vaultPasswordBox;
+
+        public VaultLoginPageViewModel ViewModel
+        {
+            get => (VaultLoginPageViewModel)DataContext;
+            set => DataContext = value;
+        }
+
         public VaultLoginPage()
         {
             InitializeComponent();
         }
 
+        public override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter is VaultLoginPageViewModel viewModel)
+                ViewModel = viewModel;
+
+            base.OnNavigatedTo(e);
+        }
+
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private async void VaultPasswordBox_OnKeyUp(object? sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                await TryUnlock();
+        }
+
+        private void VaultPasswordBox_OnLoaded(object? sender, RoutedEventArgs e)
+        {
+            _vaultPasswordBox = sender as TextBox;
+        }
+
+        private async void ContinueButton_OnClick(object? sender, RoutedEventArgs e)
+        {
+            await TryUnlock();
+        }
+
+        private void ContinueButton_OnLoaded(object? sender, RoutedEventArgs e)
+        {
+            _continueButton = sender as Button;
+        }
+
+        private async Task TryUnlock()
+        {
+            if (_continueButton?.IsEnabled ?? false)
+            {
+                if (_vaultPasswordBox is null || _continueButton?.DataContext is not LoginCredentialsViewModel viewModel)
+                    return;
+
+                _continueButton.IsEnabled = false;
+                await Task.Delay(25); // Wait for UI to update.
+
+                var securePassword = string.IsNullOrEmpty(_vaultPasswordBox.Text) ? null : new SecurePassword(Encoding.UTF8.GetBytes(_vaultPasswordBox.Text));
+                await viewModel.UnlockVaultCommand.ExecuteAsync(securePassword);
+
+                await Task.Delay(25);
+                _continueButton.IsEnabled = true;
+            }
         }
     }
 }
