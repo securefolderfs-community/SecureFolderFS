756787c2428bf7ae6a0f60f3e178bbac0b8947d3
taj_ny <79316397+taj-ny@users.noreply.github.com>
2023-01-27 16:08:48 +0100

    Implemented FUSE (#18)

---
+++ SecureFolderFS.Core.WebDav/Storage/IDavStorableInternal.cs
@@ -0,0 +1,12 @@
+﻿using NWebDav.Server.Storage;
+using SecureFolderFS.Sdk.Storage;
+using SecureFolderFS.Shared.Utils;
+
+namespace SecureFolderFS.Core.WebDav.Storage
+{
+    // TODO: Add docs
+    public interface IDavStorableInternal<out TStorableInternal> : IWrapper<TStorableInternal>, IDavStorable
+        where TStorableInternal : IStorable
+    {
+    }
+}
