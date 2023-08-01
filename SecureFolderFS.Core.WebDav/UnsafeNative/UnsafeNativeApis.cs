fe0938a73c10c07407aec839396cc46e57fb5cc2
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2023-08-01 16:52:11 +0200

    Auto-close WebDav file explorer windows

--- SecureFolderFS.Core.WebDav/UnsafeNative/UnsafeNativeApis.cs
+++ SecureFolderFS.Core.WebDav/UnsafeNative/UnsafeNativeApis.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace SecureFolderFS.Core.WebDav.UnsafeNative
@@ -8,28 +9,35 @@ namespace SecureFolderFS.Core.WebDav.UnsafeNative
         public const int CONNECT_TEMPORARY = 4;
         public const int RESOURCETYPE_DISK = 1;
 
-        [DllImport("Mpr.dll")]
+        [DllImport("Mpr.dll", CharSet = CharSet.Unicode)]
         public static extern int WNetAddConnection2(
             [In] NETRESOURCE lpNetResource,
             [In] string lpPassword,
             [In] string lpUserName,
             [In] uint dwFlags);
 
-        [DllImport("Mpr.dll")]
+        [DllImport("Mpr.dll", CharSet = CharSet.Unicode)]
         public static extern int WNetCancelConnection2(
             [In] string lpName,
             [In] uint dwFlags,
             [In] bool fForce);
 
-        [DllImport("Mpr.dll")]
+        [DllImport("Mpr.dll", CharSet = CharSet.Unicode)]
         public static extern int WNetGetConnection(
             [In] string lpLocalName,
             [Out] StringBuilder lpRemoteName,
             [In, Out] ref int lpnLength);
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessageA(
+            [In] IntPtr hWnd,
+            [In] uint wMsg,
+            [In] IntPtr wParam,
+            [In] IntPtr lParam);
     }
 
     [StructLayout(LayoutKind.Sequential)]
-    public class NETRESOURCE
+    internal class NETRESOURCE
     {
         public uint dwScope;
         public uint dwType;
@@ -39,5 +47,10 @@ namespace SecureFolderFS.Core.WebDav.UnsafeNative
         public string lpRemoteName = null!;
         public string lpComment = null!;
         public string lpProvider = null!;
+    }
+
+    internal static class WindowMessages
+    {
+        public const uint WM_CLOSE = 0x0010;
     }
 }
