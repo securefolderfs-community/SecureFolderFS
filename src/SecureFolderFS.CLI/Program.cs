e3f89dfb3fc9ccd0d0059791dca096edbbac3bbf
taj-ny <79316397+taj-ny@users.noreply.github.com>
2023-11-20 16:10:37 +0100

    Add SecureFolderFS.CLI project

---
+++ src/SecureFolderFS.CLI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SecureFolderFS.CLI
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
