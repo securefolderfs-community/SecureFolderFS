ef48561fa93bea4aaafbe430e72ed72fa0150b6e
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2024-01-01 19:39:18 +0100

    Fixed dialog service

--- src/SecureFolderFS.Uno/SecureFolderFS.Uno/ServiceImplementation/UnoDialogService.cs
+++ src/SecureFolderFS.Uno/SecureFolderFS.Uno/ServiceImplementation/UnoDialogService.cs
@@ -1,5 +1,4 @@
 using System.Threading.Tasks;
-using Microsoft.UI.Xaml.Controls;
 using SecureFolderFS.Sdk.Services;
 using SecureFolderFS.Sdk.ViewModels.Dialogs;
 using SecureFolderFS.Shared.ComponentModel;
@@ -8,13 +7,19 @@ using SecureFolderFS.UI.ServiceImplementation;
 using SecureFolderFS.UI.Utils;
 using SecureFolderFS.Uno.Dialogs;
 using SecureFolderFS.Uno.UserControls.Introduction;
+
+#if WINDOWS
+using Microsoft.UI.Xaml.Controls;
 using Windows.Foundation.Metadata;
+#endif
 
 namespace SecureFolderFS.Uno.ServiceImplementation
 {
     /// <inheritdoc cref="IOverlayService"/>
     public sealed class UnoDialogService : BaseOverlayService
     {
+        private bool _isCurrentRemoved;
+
         /// <inheritdoc/>
         protected override IOverlayControl GetOverlay(IView view)
         {
@@ -49,14 +54,26 @@ namespace SecureFolderFS.Uno.ServiceImplementation
 
             var current = Overlays.Pop();
             current.Hide();
+            _isCurrentRemoved = true;
 
-            var result = await base.ShowAsync(view);
+            var overlay = GetOverlay(view);
+            var result = await ShowOverlayAsync(overlay);
+            if (!_isCurrentRemoved && !Overlays.IsEmpty())
+                Overlays.Pop();
 
             Overlays.Push(current);
             await current.ShowAsync();
             Overlays.Pop();
+            _isCurrentRemoved = false;
 
             return result;
+
+            Task<IResult> ShowOverlayAsync(IOverlayControl overlay)
+            {
+                overlay.SetView(view);
+                Overlays.Push(overlay);
+                return overlay.ShowAsync();
+            }
         }
     }
 }
