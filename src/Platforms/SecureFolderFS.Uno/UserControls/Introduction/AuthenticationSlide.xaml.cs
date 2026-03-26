768fec508e63e56a59bb99449859d27998588b0c
d2dyno <53011783+d2dyno1@users.noreply.github.com>
2026-03-26 22:05:03 +0100

    Improved the design of AuthenticationSlide

---
+++ src/Platforms/SecureFolderFS.Uno/UserControls/Introduction/AuthenticationSlide.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SecureFolderFS.Uno.UserControls.Introduction
+{
+    public sealed partial class AuthenticationSlide : UserControl
+    {
+        public AuthenticationSlide()
+        {
+            InitializeComponent();
+        }
+    }
+}
