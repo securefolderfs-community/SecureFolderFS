939e02ee75244eebc47014fdf751eae857d3048d
d2dyno <53011783+d2dyno1@users.noreply.github.com>
2026-07-12 16:37:48 +0200

    Fixed CryptoFolder's GetCiphertextRepresentationAsync

--- src/Core/SecureFolderFS.Core.FileSystem/Validators/BaseFileSystemValidator.cs
+++ src/Core/SecureFolderFS.Core.FileSystem/Validators/BaseFileSystemValidator.cs
@@ -1,5 +1,4 @@
 ﻿using OwlCore.Storage;
-using SecureFolderFS.Core.Cryptography;
 using SecureFolderFS.Core.FileSystem.Exceptions;
 using SecureFolderFS.Core.FileSystem.Helpers.Paths.Abstract;
 using SecureFolderFS.Shared.ComponentModel;
@@ -71,16 +70,7 @@ namespace SecureFolderFS.Core.FileSystem.Validators
             if (!string.IsNullOrEmpty(decryptedName))
                 return decryptedName;
 
-            // A shortened file (.sffsn) that couldn't be decrypted means its sidecar is missing.
-            // Report this as an invalid name so the health system can offer to generate a new one.
-            if (storable.Name.EndsWith(Constants.Names.SHORTENED_FILE_EXTENSION, StringComparison.OrdinalIgnoreCase))
-                return null;
-
-            // We want to suppress failures that might be raised when the Directory ID file is not found.
-            // This case should be already handled in the folder validator
-
-            // Return an empty string to prevent raising exceptions due to the name being null
-            return string.Empty;
+            return null;
         }
     }
 }
