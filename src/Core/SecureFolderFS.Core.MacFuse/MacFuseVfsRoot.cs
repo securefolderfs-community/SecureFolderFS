0ea3374a7649c2e56571789596e73fdd0b20bc4b
d2dyno <53011783+d2dyno1@users.noreply.github.com>
2026-07-18 23:50:42 +0200

    Added base implementation classes for MacFuse

---
+++ src/Core/SecureFolderFS.Core.MacFuse/MacFuseVfsRoot.cs
@@ -0,0 +1,36 @@
+using OwlCore.Storage;
+using SecureFolderFS.Core.FileSystem;
+using SecureFolderFS.Storage.VirtualFileSystem;
+
+namespace SecureFolderFS.Core.MacFuse
+{
+    /// <inheritdoc cref="IVfsRoot"/>
+    internal sealed class MacFuseVfsRoot : VfsRoot
+    {
+        private readonly MacFuseWrapper _fuseWrapper;
+        private bool _disposed;
+
+        /// <inheritdoc/>
+        public override string FileSystemName { get; } = Constants.FileSystem.FS_NAME;
+
+        public MacFuseVfsRoot(MacFuseWrapper fuseWrapper, IFolder virtualizedRoot, IFolder plaintextRoot, FileSystemSpecifics specifics)
+            : base(virtualizedRoot, plaintextRoot, specifics)
+        {
+            _fuseWrapper = fuseWrapper;
+        }
+
+        /// <inheritdoc/>
+        public override async ValueTask DisposeAsync()
+        {
+            if (_disposed)
+                return;
+
+            _disposed = await _fuseWrapper.CloseFileSystemAsync();
+            if (_disposed)
+            {
+                FileSystemManager.Instance.FileSystems.Remove(this);
+                await base.DisposeAsync();
+            }
+        }
+    }
+}
